c40eea4476ca99918b81906dfcfaee4039b33030
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-07-23 07:11:12 +0300

    05-list-rendering

--- main.js
+++ main.js
@@ -4,7 +4,20 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 11,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [
+                {id: 1, name: 'XS'},
+                {id: 2, name: 'S'},
+                {id: 3, name: 'M'},
+                {id: 4, name: 'L'},
+                {id: 5, name: 'XL'},
+            ]
+
         }
     }
 })
